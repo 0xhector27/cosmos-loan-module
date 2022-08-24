3a52d772426dc06ac0830f9bf60c51feb0608440
SmartCoding51 <topstar51@outlook.com>
2022-08-24 01:31:33 -0700

    request loan message

--- vue/src/store/generated/loan/loan.loan/module/rest.ts
+++ vue/src/store/generated/loan/loan.loan/module/rest.ts
@@ -21,6 +21,8 @@ export interface LoanLoan {
   lender?: string;
 }
 
+export type LoanMsgRequestLoanResponse = object;
+
 /**
  * Params defines the parameters for the module.
  */
@@ -101,6 +103,13 @@ export interface V1Beta1PageRequest {
    * is set.
    */
   count_total?: boolean;
+
+  /**
+   * reverse is set to true if results are to be returned in the descending order.
+   *
+   * Since: cosmos-sdk 0.43
+   */
+  reverse?: boolean;
 }
 
 /**
@@ -330,6 +339,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
       "pagination.offset"?: string;
       "pagination.limit"?: string;
       "pagination.count_total"?: boolean;
+      "pagination.reverse"?: boolean;
     },
     params: RequestParams = {},
   ) =>
